42899b7a52aea56fcbc24513bd192fef058d4611
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-13 02:48:37 +0530

    Added style to icons

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { View } from "react-native";
-import {
-  FontAwesome,
-  MaterialIcons,
-  MaterialCommunityIcons
-} from "@expo/vector-icons";
-import { black } from "./colors";
+import { View, StyleSheet } from 'react-native'
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { red, orange, blue, lightPurp, pink } from './colors'
 
 export function getDailyReminderValue () {
   return {
     today: "👋 Don't forget to log your data today!"
   }
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20
+  },
+})
 
 export function getMetricMetaInfo(metric) {
     const info = {
@@ -22,11 +30,11 @@ export function getMetricMetaInfo(metric) {
             step: 1,
             type: "steppers",
             getIcon() {
-                return (
-                <View>
-                    <MaterialIcons name="directions-run" color={'black'} size={35} />
+              return (
+                <View style={[styles.iconContainer, {backgroundColor: red}]}>
+                  <MaterialIcons name="directions-run" color={'white'} size={35} />
                 </View>
-                );
+              );
             }
         },
         bike: {
@@ -37,9 +45,9 @@ export function getMetricMetaInfo(metric) {
             type: "steppers",
             getIcon() {
                 return (
-                <View>
-                    <MaterialCommunityIcons name="bike" color={'black'} size={32} />
-                </View>
+                  <View style={[styles.iconContainer, {backgroundColor: orange}]}>
+                      <MaterialCommunityIcons name="bike" color={'white'} size={32} />
+                  </View>
                 );
             }
         },
@@ -51,9 +59,9 @@ export function getMetricMetaInfo(metric) {
             type: "steppers",
             getIcon() {
                 return (
-                <View>
-                    <MaterialCommunityIcons name="swim" color={'black'} size={35} />
-                </View>
+                  <View style={[styles.iconContainer, {backgroundColor: blue}]}>
+                      <MaterialCommunityIcons name="swim" color={'white'} size={35} />
+                  </View>
                 );
             }
         },
@@ -65,9 +73,9 @@ export function getMetricMetaInfo(metric) {
             type: "slider",
             getIcon() {
                 return (
-                <View>
-                    <FontAwesome name="bed" color={'black'} size={30} />
-                </View>
+                  <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
+                      <FontAwesome name="bed" color={'white'} size={30} />
+                  </View>
                 );
             }
         },
@@ -79,9 +87,9 @@ export function getMetricMetaInfo(metric) {
             type: "slider",
             getIcon() {
                 return (
-                <View>
-                    <MaterialCommunityIcons name="food" color={'black'} size={35} />
-                </View>
+                  <View style={[styles.iconContainer, {backgroundColor: pink}]}>
+                      <MaterialCommunityIcons name="food" color={'white'} size={35} />
+                  </View>
                 );
             }
         }

15e88fdcf41a62f06c45cf6d90e56e87138292ad
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-15 05:25:09 +0530

    Fixed bug for Add entry page

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -196,7 +196,7 @@ function mapStateToProps(state) {
   const key = timeToString();
 
   return {
-    alreadyLogged: state[key] && typeof state[key].today === "undefined"
+    alreadyLogged: state[key] && typeof state[key][0].today === "undefined"
   };
 }
 
